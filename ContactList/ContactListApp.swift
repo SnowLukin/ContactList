f06e9589fe3b95f90aff546b9255742dc1edccbb
Snow Lukin <everyoursden@gmail.com>
2022-03-07 17:33:16 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Snow Lukin on 07.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
